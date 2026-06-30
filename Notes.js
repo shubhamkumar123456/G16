fc3ab9dd03b16a880cc1ab31d4ddc3d0af3e1d65
shubham kumar <shubhamfarainzi@gmail.com>
2026-06-30 17:35:15 +0530

    first commit

---
+++ Notes.js
@@ -0,0 +1,13 @@
+
+// HTML -->
+
+// ! --> emmet --> generate basic html code(boiler plat code) --> shit + 1
+// comment tag  --> ctrl + ?
+
+// dummt text --> lorem30
+
+// text wrap --> alt + z
+
+// tag -->  
+
+{/* <openingTag attribute="value">  content   </closingTag> */}
