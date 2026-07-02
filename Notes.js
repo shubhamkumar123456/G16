4b8890b34138853a840f56610b5feb3b0e30a586
shubham kumar <shubhamfarainzi@gmail.com>
2026-07-02 16:54:27 +0530

    updated all

--- Notes.js
+++ Notes.js
@@ -2,6 +2,7 @@
 // HTML -->
 
 // ! --> emmet --> generate basic html code(boiler plat code) --> shit + 1
+
 // comment tag  --> ctrl + ?
 
 // dummt text --> lorem30
@@ -11,3 +12,27 @@
 // tag -->  
 
 {/* <openingTag attribute="value">  content   </closingTag> */}
+
+
+
+
+
+// CSS --> 
+
+// 1) inline css --> we use style attribute inside any tag example -->
+//  <u style="color: blue; background-color: burlywood;">this is bold tag</u>
+
+//2) Internal Css --> we use style tag inside head tag example -->
+
+{/* <head>
+    <style>
+        p{
+            color: aqua;
+            background-color: black;
+        }
+        h1{
+            color: blueviolet;
+            background-color: yellow;
+        }
+    </style>
+</head> */}
