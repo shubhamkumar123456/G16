8c3c6025d49c9a10b217c2183379ce365394dd55
shubham kumar <shubhamfarainzi@gmail.com>
2026-09-22 16:04:07 +0530

    added tailwind in ecom project

--- react/ecom/src/components/Navbar.jsx
+++ react/ecom/src/components/Navbar.jsx
@@ -3,8 +3,9 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
-      <ul>
+    <div className='bg-yellow-700 px-5 text-white justify-between h-[60px] flex items-center'>
+      <h1>Ecom Web</h1>
+      <ul className='flex gap-5 items-center bg-black'>
         <li> <Link to={'/'}>Home</Link> </li>
         <li> <Link to={'/about'}>About</Link> </li>
         <li> <Link to={'/cart'}>Cart</Link> </li>
